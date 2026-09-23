e7bf34ab5f1d99097fa8b256cedc5007c9d32deb
Anthony Valenzuela <N00471400@upn.pe>
2026-09-23 12:46:10 -0500

    Promedio de todas las notas

--- Program.cs
+++ Program.cs
@@ -14,7 +14,3 @@ for (int i = 0; i < notas.Length; i++)
     suma += notas[i];
 }
 Console.WriteLine($"Suma de todas las notas: {suma}");
-
-//Promedio de todas las notas
-double promedio = (double)suma / notas.Length;
-Console.WriteLine($"Promedio de todas las notas: {promedio:F2}");
