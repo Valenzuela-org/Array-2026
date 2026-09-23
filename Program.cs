16e54facf410f447a324270926a42999bebe6fe7
Anthony Valenzuela <N00471400@upn.pe>
2026-09-23 13:12:56 -0500

    Mayor y menor

--- Program.cs
+++ Program.cs
@@ -23,3 +23,22 @@ imprimirNotas(notas);
 //Promedio de todas las notas
 double promedio = calcularPromedio(notas);
 Console.WriteLine($"Promedio de todas las notas: {promedio:F2}");
+
+//para hallar la nota mas alta y la mas baja
+int notaMasAlta = notas[0];
+int notaMasBaja = notas[0];
+
+for (int i = 1; i < notas.Length; i++)
+{
+    if (notas[i] > notaMasAlta)
+    {
+        notaMasAlta = notas[i];
+    }
+    if (notas[i] < notaMasBaja)
+    {
+        notaMasBaja = notas[i];
+    }
+}
+
+Console.WriteLine($"Nota más alta: {notaMasAlta}");
+Console.WriteLine($"Nota más baja: {notaMasBaja}");
