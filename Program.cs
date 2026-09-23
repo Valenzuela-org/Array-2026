53b7eac64ec622281d43036c9a31dca85d11f6b8
Anthony Valenzuela <N00471400@upn.pe>
2026-09-23 12:44:08 -0500

    Suma de las notas

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@
 }
 int[] notas = {15,18,12,17,20};
 imprimirNotas(notas);
+//Suma de todas las notas
+int suma = 0; 
+for (int i = 0; i < notas.Length; i++)
+{
+    suma += notas[i];
+}
+Console.WriteLine($"Suma de todas las notas: {suma}");
+
+//Promedio de todas las notas
+double promedio = (double)suma / notas.Length;
+Console.WriteLine($"Promedio de todas las notas: {promedio:F2}");
