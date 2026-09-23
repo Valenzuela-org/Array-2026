82f4dad69f09df666989734fe08607db21d06d14
Anthony Valenzuela <N00471400@upn.pe>
2026-09-23 13:24:14 -0500

    Mayor y menor en funcion

--- Program.cs
+++ Program.cs
@@ -23,22 +23,32 @@ imprimirNotas(notas);
 //Promedio de todas las notas
 double promedio = calcularPromedio(notas);
 Console.WriteLine($"Promedio de todas las notas: {promedio:F2}");
-
-//para hallar la nota mas alta y la mas baja
-int notaMasAlta = notas[0];
-int notaMasBaja = notas[0];
-
-for (int i = 1; i < notas.Length; i++)
+//Funcion para maximos
+static int hallarNotaMasAlta(int[] notas)
 {
-    if (notas[i] > notaMasAlta)
+    int notaMasAlta = notas[0];
+    for (int i = 1; i < notas.Length; i++)
     {
-        notaMasAlta = notas[i];
+        if (notas[i] > notaMasAlta)
+        {
+            notaMasAlta = notas[i];
+        }
     }
-    if (notas[i] < notaMasBaja)
-    {
-        notaMasBaja = notas[i];
-    }
+    return notaMasAlta;
 }
 
-Console.WriteLine($"Nota más alta: {notaMasAlta}");
-Console.WriteLine($"Nota más baja: {notaMasBaja}");
+//Funcion para minimos
+static int hallarNotaMasBaja(int[] notas)
+{
+    int notaMasBaja = notas[0];
+    for (int i = 1; i < notas.Length; i++)
+    {
+        if (notas[i] < notaMasBaja)
+        {
+            notaMasBaja = notas[i];
+        }
+    }
+    return notaMasBaja;
+}
+Console.WriteLine($"Nota más alta: {hallarNotaMasAlta(notas)}");
+Console.WriteLine($"Nota más baja: {hallarNotaMasBaja(notas)}");
--- obj/Debug/net10.0/Test.AssemblyInfo.cs
+++ obj/Debug/net10.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec95d2fd8321594523eb417f10a3a609db17613")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e54facf410f447a324270926a42999bebe6fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
