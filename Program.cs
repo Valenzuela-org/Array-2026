ce84ed8797659f72a8137da375eeb12a55cee1ac
Anthony Valenzuela <N00471400@upn.pe>
2026-09-23 12:37:38 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿static void imprimirNotas(int []notasx)
+{
+    for (int i=0;i<notasx.Length;i++)
+    {
+        Console.WriteLine($"Nota {i+1}; {notasx[i]}");
+    }
+}
+int[] notas = {15,18,12,17,20};
+imprimirNotas(notas);
